d2679cf8bb8fd9d71f7c7000d37862012110c353
KAVITHIVAN <kavithivanganeson@gmail.com>
2022-09-20 12:17:11 +0800

    ok

---
+++ app/src/main/java/com/example/loginactivity/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.loginactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etName;
+    Button btnName;
+
+    public final static String MESSAGE_KEY = "com.example.LoginActivity.message_key";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void send_data(View view){
+        etName = findViewById(R.id.etName);
+        btnName = findViewById(R.id.btnName);
+        String message = etName.getText().toString();
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(MESSAGE_KEY,message);
+        startActivity(intent);
+
+    }
+}
